67684aae4a666343d56a1d6f385de936fa27f94c
pieetrus <pietrusjakub@gmail.com>
2024-08-15 12:24:48 +0200

    first commit

---
+++ src/models.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TrafficIncidentResponse {
+    pub incidents: Vec<Incident>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Incident {
+    #[serde(rename = "type")]
+    pub incident_type: String,
+    pub properties: IncidentProperties,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct IncidentProperties {
+    pub id: String,
+    #[serde(rename = "iconCategory")]
+    pub icon_category: u8,
+    #[serde(rename = "magnitudeOfDelay")]
+    pub magnitude_of_delay: u8,
+    pub events: Vec<Event>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Event {
+    pub code: u16,
+    pub description: String,
+}
